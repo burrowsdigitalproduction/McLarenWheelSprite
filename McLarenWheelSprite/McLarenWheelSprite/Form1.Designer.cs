51e50cd1a0bbec50aaa843275c58be86a7dee55f
Burrows Digital Production <burrowsdigitalproduction@gmail.com>
2019-03-18 12:13:46 +0000

    Add files via upload

---
+++ McLarenWheelSprite/McLarenWheelSprite/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace McLarenWheelSprite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(18, 16);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(834, 20);
+            this.txtFolder.TabIndex = 0;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(870, 12);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnFolder.TabIndex = 1;
+            this.btnFolder.Text = "Folder";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(870, 109);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 49);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 170);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ McLarenWheelSprite/McLarenWheelSprite/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace McLarenWheelSprite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string folderLocation = @"";
+        List<FileInfo> files = new List<FileInfo>();
+        List<FileInfo> filesRemove = new List<FileInfo>();
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fldDlg = new FolderBrowserDialog();
+            fldDlg.ShowNewFolderButton = false;
+            DialogResult result = fldDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFolder.Text = fldDlg.SelectedPath;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Part1();
+        }
+        public void renewList()
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderLocation);
+
+            files = dir.GetFiles().ToList();
+
+            
+        }
+
+        public void Part1()
+        {
+
+            try {
+
+                
+
+                folderLocation = txtFolder.Text;
+
+                renewList();
+
+                if (files.Count == 0)
+            {
+                MessageBox.Show("No files in folder", "", MessageBoxButtons.OK);
+                return;
+            }
+
+            
+
+            int i = 0;
+
+            foreach (FileInfo f in files)
+            {
+                if (i > 1)
+                {
+                    i = 0;
+                }
+                if (i == 1)
+                {
+                    filesRemove.Add(f);
+                }
+                i++;
+                
+            }
+            foreach (FileInfo fr in filesRemove)
+            {
+                File.Delete(fr.FullName);
+            }
+            filesRemove.Clear();
+
+            Part2();
+            }
+            catch (System.Exception excep)
+            {
+                MessageBox.Show(excep.Message, "", MessageBoxButtons.OK);
+            }
+
+        }
+
+        public void Part2()
+        {
+            try
+            {
+
+            
+            renewList();
+
+            string pname1 = "02";
+            string pname2 = "06";
+            string pname3 = "010";
+
+            FileInfo p1 = files.FirstOrDefault(s => s.FullName.Contains(pname1));
+            FileInfo p2 = files.FirstOrDefault(s => s.FullName.Contains(pname2));
+            FileInfo p3 = files.FirstOrDefault(s => s.FullName.Contains(pname3));
+
+            File.Delete(p1.FullName);
+            File.Delete(p2.FullName);
+            File.Delete(p3.FullName);
+            filesRemove.Clear();
+
+            Part3();
+
+            }
+            catch (System.Exception excep)
+            {
+                MessageBox.Show(excep.Message, "", MessageBoxButtons.OK);
+            }
+        }
+
+        public void Part3()
+        {
+
+            try
+            {
+
+            
+            renewList();
+
+            int n = 26;
+
+            foreach (FileInfo fr2 in files)
+            {
+                
+                string nr = n.ToString();
+
+                if (fr2.FullName.Contains(nr))
+                {
+                    filesRemove.Add(fr2);
+
+                    n++;
+                    n++;
+                    n++;
+                    n++;
+                }
+                
+            }
+
+            foreach (FileInfo flr in filesRemove)
+            {
+                File.Delete(flr.FullName);
+
+            }
+
+            MessageBox.Show("All Done", "All Done", MessageBoxButtons.OK);
+
+            }
+            catch (System.Exception excep)
+            {
+                MessageBox.Show(excep.Message, "", MessageBoxButtons.OK);
+            }
+        }
+
+        public void DoNothing()
+        {
+
+        }
+
+        
+    }
+}
